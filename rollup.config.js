79a5c77af0a9a29e9152d6b0562c268c3a35f13c
Matthew Rollinson <matt@clay10.co.uk>
2019-10-24 13:05:00 +0100

    Added rollup

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import pkg from "./package.json";
+import vue from "rollup-plugin-vue";
+import commonjs from "rollup-plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs"
+    },
+    {
+      file: pkg.module,
+      format: "es"
+    }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+  ],
+  plugins: [
+    commonjs(),
+    typescript({
+      clean: true,
+      objectHashIgnoreUnknownHack: true
+    }),
+    vue(),
+    terser()
+  ]
+};
