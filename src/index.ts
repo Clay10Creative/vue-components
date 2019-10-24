5c45a59b6b83299447408907134a40f86165164e
Matthew Rollinson <matt@clay10.co.uk>
2019-10-24 13:25:55 +0100

    Version 3.0.0

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,22 @@ import CTPaginate from "./components/CTPaginate.vue";
 import CTSelect from "./components/CTSelect.vue";
 import CTSlider from "./components/CTSlider.vue";
 import CTTooltip from "./components/CTTooltip.vue";
-
 import { IComponents } from "./types";
 
+export * from "./types";
+
 export const Components: IComponents = {
-  CTAnimatedNumber,
-  CTAnimation,
-  CTInput,
-  CTPaginate,
-  CTSelect,
-  CTSlider,
-  CTTooltip
+  "ct-animated-number": CTAnimatedNumber,
+  "ct-animation": CTAnimation,
+  "ct-input": CTInput,
+  "ct-paginate": CTPaginate,
+  "ct-select": CTSelect,
+  "ct-slider": CTSlider,
+  "ct-tooltip": CTTooltip
 };
 
 export default {
-  install(Vue: VueConstructor) {
+  install(Vue: VueConstructor<any>) {
     Object.keys(Components).forEach((name: string) => {
       Vue.component(name, Components[name]);
     });
